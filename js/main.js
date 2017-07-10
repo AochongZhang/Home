ab4eb551bfcf479e5e834143a8d001bfafb46b2b
Aochong Zhang <mishrimp@163.com>
2017-07-10 16:37:10 +0800

    右侧网站区域自动居中
    使用@media使右侧网站区域有固定宽度值，从而达到自动居中目的。

--- js/main.js
+++ js/main.js
@@ -9,14 +9,24 @@ $(function () {
   })
 })
 $(function () {
-  var hight = $(window).height();
-  var left = $(".side-bar").width();
-  $(".side-bar").height(hight);
-  $(".content").height(hight).css("left",left);
+  content();
 })
 window.onresize = function () {
-  var hight = $(window).height();
-  var left = $(".side-bar").width();
-  $(".side-bar").height(hight);
-  $(".content").height(hight).css("left", left);
+  content();
+}
+function content() {
+  var height = window.innerHeight;  //使用jQuery方法出现BUG,改为原生方法解决。
+  var width = window.innerWidth;
+  var bwidth = $(".side-bar").width();
+  $(".side-bar").height(height);
+  $(".content").width(width-bwidth).height(height);
+  // console.log("window-widht   :"+width);
+  // console.log("window-height  :"+height);
+  // console.log("side-bar-width :"+bwidth);
+  // console.log("side-bar-height:"+$(".side-bar").height());
+  // console.log("content-width  :"+$(".content").width());
+  // console.log("width-bwidth   :"+(width-bwidth));
+  // console.log("content-height :"+$(".content").height());
+  // console.log("------------------------");
+
 }
