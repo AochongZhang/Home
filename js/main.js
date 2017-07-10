7c3d1cbd8490c01cbbdaf9c7242e23869a26c410
Aochong Zhang <mishrimp@163.com>
2017-07-10 16:39:53 +0800

    add

--- js/main.js
+++ js/main.js
@@ -15,8 +15,21 @@ $(function () {
   $(".content").height(hight).css("left",left);
 })
 window.onresize = function () {
-  var hight = $(window).height();
-  var left = $(".side-bar").width();
-  $(".side-bar").height(hight);
-  $(".content").height(hight).css("left", left);
+  var height = $(window).height();
+  var width = $(window).width();
+  var bwidth = $(".side-bar").width();
+  var content = $(".content");
+  var websidewrap = $(".webside-wrap");
+  var cwidth = $(".content").width();
+  $(".side-bar").height(height);
+  $(".content").css("left",bwidth).width(width-bwidth).height(height);
+  if (cwidth>=1040) {
+    websidewrap.width(1040);
+  }else if (cwidth<1040&&cwidth>=780) {
+    websidewrap.width(780);
+  }else if (cwidth<780&&cwidth>=520) {
+    websidewrap.width(520);
+  }else if (cwidth<520&&cwidth>=260) {
+    websidewrap.width(260);
+  }
 }
